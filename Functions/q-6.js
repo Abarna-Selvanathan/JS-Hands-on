3a134b9d0fa880bbab50fd06c458b41214c3fa42
Abarna Selvanathan <rnaaba21@gmail.com>
2024-10-07 20:58:16 +0530

    added function

---
+++ Functions/q-6.js
@@ -0,0 +1,6 @@
+function area(r,r){
+    var areaofcricle=3.14*r*r
+    return areaofcricle
+}
+let value=area(2,2)
+console.log(value)
---
+++ Functions/q-8.js
@@ -0,0 +1,6 @@
+function clesiustofahrenheit(celsius){
+    var value=(celsius*9/5)+32
+    return value
+}
+let temperature=clesiustofahrenheit(45)
+console.log(temperature)
